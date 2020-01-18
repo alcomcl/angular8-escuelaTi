1907769bb3e68caa0fc28d5f1f7e889259807964
alcomcl <alex.cofre.martinez@gmail.com>
2020-01-18 19:11:08 -0300

    Primera Clase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Aprendiendo';
+  public title = 'Primera Clase';
+  public name = 'Alex Cofré';
+  public rockBandName: string = '';
+  public rockBands: string[] = ['Los Prisioneros', 'Los Cadilacs', 'Git'];
+
+
+  changeTitle() {
+    if (this.title === 'Primera Clase') {
+      this.title = 'Hola Alex';
+    } else {
+      this.title = 'Primera Clase';
+    }
+  }
+
+  changeName() {
+    if (this.name === 'Nataly'){
+      this.name = 'Alex Cofré';
+    } else {
+      this.name = 'Nataly';
+    }
+  }
+
+  addRockBand(){
+    this.rockBands.push(this.rockBandName);
+    this.rockBandName = '';
+  }
+
+
 }
